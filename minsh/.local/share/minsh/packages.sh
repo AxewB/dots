22fa510f6122a2719c04695c861c47f46938cb99
AxewB <15068616+AxewB@users.noreply.github.com>
2026-09-06 01:21:36 +0300

    added flavours and own scripts whick i need to cleanup later

---
+++ minsh/.local/share/minsh/packages.sh
@@ -0,0 +1,152 @@
+# system
+ntfs-3g
+chntpw # util to read registry (used for adding same BT device on different OS but same PC)
+
+vim
+bob # neovim # while vim.pack in nightly I use bob-nvim
+wget
+git
+
+docker        # containers
+diff-so-fancy # better diff for git
+
+# Hyprland
+uwsm # universal wayland session manager
+hyprland
+hyprsunset
+hyprcursor
+hyprshutdown # AUR
+hyprlock
+hyprshot
+hypridle
+hyprpicker
+
+# HYPRLAND PLUGINS
+# hyprpm update
+# hyprpm add https://github.com/hyprwm/hyprland-plugins
+# hyprpm add https://github.com/VirtCode/hypr-dynamic-cursors
+# hyprpm enable hyprbars
+# hyprpm enable dynamic-cursors
+
+# apps
+kitty # terminal
+firefox
+helium-browser-bin # AUR
+foot               # alternative terminal
+mako               # notification daemon
+libnotify          # notif daemon dependency
+zathura            # PDF viewer
+zathura_pdf_mupdf  # plugin to actually view PDFs
+zathura_djvu       # djvu support
+zathura_cb         # comic book support
+dolphin            # file manager
+dolphin-plugins
+vimiv
+thunderbird
+localsend
+
+syncthing              # local syncing
+chatterino2            # chat for twitch
+obs-studio             # obs
+telegram-desktop       # telegram
+obsidian               # markdown note taking
+discord                # discodo
+zed                    # text editor
+code                   # just in case
+network-managerlapplet # tray thingy for NM
+
+# terminal utils
+fzf          # fuzzy finder
+fd           # better 'find'
+zoxide       # better 'cd'
+bat          # better 'cat'
+eza          # better 'ls'
+jq           # json formatter cli
+ripgrep      # better 'grep'
+tldr         # simpler 'man' pages
+tmux         # terminal multiplexer
+zellij       # modern terminal multiplexer
+just         # tool to save and run project-specific commands
+ddcutil      # control monitor brightness
+wl-clipboard # copy and paste with pipe on wayland
+ffmpeg       # operatios with auido and video files
+imagemagick  # operations with images (e.g. conversion)
+starship     # prompt styling
+gum          # creating nice looking terminal actions
+playerctl    # controlling system players
+
+# must have
+pavucontrol
+mpv        # video viewer
+gwenview   # image viewer
+quickshell # developing shell with QML
+
+#
+qbittorrent
+
+# TUI's
+btop       # resource monitor
+yazi       # file manager TUI
+lazygit    # git TUI
+lazydocker # docker TUI
+bluetui    # bluetooth TUI
+wiremix    # volume TUI
+rmpc       # music player
+mpd        # daemon for music player
+
+# Editors
+kdenlive # video editor
+# blender # modeling
+# krita # drawing
+libreoffice-still # office suite
+
+# Theming
+nwg-look
+
+# fonts
+ttf-dejavu
+ttf-dejavu-nerd
+ttf-cascadia-code-nerd
+ttf-roboto
+ttf-roboto-mono-nerd
+
+flavours # base16 colorscheme generation
+python-pywal
+python-pywalfox # AUR # theming firefox dynamically
+
+kvantum # qt app theming
+qt5ct
+qt6ct
+qt6-svg
+qt5-svg
+
+bibata-cursor-theme
+rose-pine-hyprcursor
+rose-pine-cursor
+papirus-icon-theme
+television
+wlr-which-key
+
+# just visuals
+cava # audio visuals
+
+satty  # screenshot annotation tool
+lolcat # coloring text in terminal (used for splash screen in some menu actions)
+figlet # for symbols?
+toilet # for symbols?
+
+# testing, maybe remove later
+amberol
+nautilus
+
+# other OS
+waydroid    # android emulation
+winboat-bin # windows emulation
+
+### development
+rust
+typst
+gcc
+cmake
+lua
+slint-tools # slint
