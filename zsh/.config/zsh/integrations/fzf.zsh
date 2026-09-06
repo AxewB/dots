9cde4fba10a86eb345c13c19211704b3d73bf58e
AxewB <15068616+AxewB@users.noreply.github.com>
2026-09-06 14:00:04 +0300

    moved theme to data dir, updated template, removed extra templates

--- zsh/.config/zsh/integrations/fzf.zsh
+++ zsh/.config/zsh/integrations/fzf.zsh
@@ -2,12 +2,17 @@
 if command -v fd >/dev/null; then
   export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
 fi
-# start flavours
+
+# default theme
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
 " --color=bg+:#3b393c,bg:#262427,spinner:#aee8f4,hl:#49cae4"\
 " --color=fg:#7c7b7d,header:#49cae4,info:#ffca58,pointer:#aee8f4"\
 " --color=marker:#aee8f4,fg+:#eae9eb,prompt:#ffca58,hl+:#49cae4"
-# end flavours
+
+# load from data directory
+if [[ -f ~/.local/share/zsh/fzf_theme.sh ]]; then
+  source ~/.local/share/zsh/fzf_theme.sh
+fi
 
 if command -v fzf >/dev/null; then
   alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
