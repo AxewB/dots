f7cffaa023125fb976ad9922ddd775047bb3ec24
AxewB <15068616+AxewB@users.noreply.github.com>
2026-09-06 13:08:23 +0300

    changed some eza aliases

--- zsh/.config/zsh/integrations/eza.zsh
+++ zsh/.config/zsh/integrations/eza.zsh
@@ -1,9 +1,9 @@
 # eza replaces ls if available
 if command -v eza >/dev/null; then
   alias tree="eza --tree"
-  alias ls='eza -l --icons=always --group-directories-first'
-  alias lt='eza --tree --level=2 --long --icons --git'
+  alias ll='eza -l --icons=always --group-directories-first' # long
+  alias lt='eza --tree --level=3 --icons --git'
   alias la='ls -a'
   alias lta='lt -a'
-  alias lc='eza --grid --icons=always --group-directories-first' # compact view
+  alias ls='eza --grid --icons=always --group-directories-first' # compact view
 fi
