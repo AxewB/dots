1074c1c16ed969f7b34052bec04a07eb907e1115
AxewB <15068616+AxewB@users.noreply.github.com>
2026-09-10 15:53:29 +0300

    cleared aliases, added alias for neovim and eza

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -9,11 +9,10 @@ alias ....='cd ../../..'
 # helix
 alias hx='helix'
 
-# flavours config
-alias minsh_flavours="flavours -d $HOME/.local/share/minsh/config/flavours/data -c $HOME/.local/share/minsh/config/flavours/config.toml"
-
 # clear
 alias cl="clear"
 
 # call fd and fzf to cd into directory
 alias cddir='cd "$(fd --type directory | fzf)"'
+
+alias n="nvim"
--- zsh/.config/zsh/integrations/eza.zsh
+++ zsh/.config/zsh/integrations/eza.zsh
@@ -2,6 +2,7 @@
 if command -v eza >/dev/null; then
   alias tree="eza --tree"
   alias ll='eza -l --icons=always --group-directories-first' # long
+  alias l='ll' # long
   alias lt='eza --tree --level=3 --icons --git'
   alias la='ls -a'
   alias lta='lt -a'
