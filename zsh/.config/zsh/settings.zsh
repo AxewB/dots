145da57cb295d436f634297b0d7cbd22dcbedd3d
AxewB <15068616+AxewB@users.noreply.github.com>
2026-09-11 12:21:33 +0300

    added better completion with TAB

--- zsh/.config/zsh/settings.zsh
+++ zsh/.config/zsh/settings.zsh
@@ -1,3 +1,3 @@
 # Disabling case-sensitivity for TAB autocomplete
 autoload -Uz compinit && compinit
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|?=**' 'l:|=*' 'r:|=*'
